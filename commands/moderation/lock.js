445526a040a8601a2e23bb2ca9360b7fbdb372ba
HangDDev <155066834+HangDDev@users.noreply.github.com>
2024-01-02 21:50:30 +0800

    Added two simple channel moderation commands.

---
+++ commands/moderation/lock.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("lock")
+    .setDescription("Lock a channel.")
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel to lock.")
+        .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const channel =
+      interaction.options.getChannel("channel") || interaction.channel;
+
+    await channel.permissionOverwrites.edit(
+      interaction.guild.roles.cache.find((role) => role.name === "@everyone"), { SendMessages: false }
+    );
+
+    const embed = interaction.client.embed(
+      { description: "**🔒 Channel Locked**" },
+      { name: `🔗 Channel`, value: `${channel} (${channel.name} - ${channel.id})` }
+    );
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
---
+++ commands/moderation/unlock.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("unlock")
+    .setDescription("Unlock a channel.")
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel to unlock.")
+        .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const channel =
+      interaction.options.getChannel("channel") || interaction.channel;
+
+    await channel.permissionOverwrites.edit(
+      interaction.guild.roles.cache.find((role) => role.name === "@everyone"), { SendMessages: true }
+    );
+
+    const embed = interaction.client.embed(
+      { description: "**🔒 Channel Unlocked**" },
+      { name: `🔗 Channel`, value: `${channel} (${channel.name} - ${channel.id})` }
+    );
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
