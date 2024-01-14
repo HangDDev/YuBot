aebcc3975fd58ad203733c83678273097e2f3089
HangDDev <155066834+HangDDev@users.noreply.github.com>
2024-01-14 18:15:26 +0800

    Removed EmbedBuilder

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+const { SlashCommandBuilder } = require("discord.js")
 const moment = require("moment");
 require("moment-duration-format");
 
