98c31c280d329810d2f31e64bca7e76f3468a2d6
HangDDev <155066834+HangDDev@users.noreply.github.com>
2024-01-25 00:27:30 +0800

    Fixed unhandled promise rejection

--- events/message/chatbot.js
+++ events/message/chatbot.js
@@ -1,36 +1,42 @@
-const { Events } = require("discord.js")
+const { Events } = require("discord.js");
 const RsnChat = require("rsnchat");
-const config = require("../../config/config.json")
+const config = require("../../config/config.json");
 const rsnchat = new RsnChat(config.api.gpt);
-const ChatBot = require("../../schema/chatbot.js")
+const ChatBot = require("../../schema/chatbot.js");
 
 module.exports = {
-    name: Events.MessageCreate,
-    once: false,
-    async execute(message) {
-        if (!message.guild || message.author.bot) return
-        const channel = await ChatBot.findOne({ Guild: message.guild.id, Channel: message.channel.id })
-        if (!channel) return
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message) {
+    if (!message.guild || message.author.bot) return;
+    const channel = await ChatBot.findOne({
+      Guild: message.guild.id,
+      Channel: message.channel.id,
+    });
+    if (!channel) return;
 
-        await message.channel.sendTyping()
+    await message.channel.sendTyping();
 
-        try {
-            rsnchat.gpt(message).then(async (response) => {
-              if (response.success) {
-                  const embed = message.client.simpleEmbed({
-                    author: true,
-                    description: response.message,
-                  })
-              message.reply({ embeds: [embed] })
-              } else {
-                  await message.reply(`${message.client.emoji.warn} | I am sorry, I could not connect to my chat API. This might be due to a network issue or a temporary outage. Please try again later. You may contact my developer in [support server](${message.client.config.links.support}). Thank your for your patience and understanding.`);
-              }
-            });
-          } catch (error) {
-            console.error(error);
-            await message.reply(
-              `${message.client.emoji.warn} | I am sorry, something went wrong. This might due to errors in my code or an API error. Please try again later or contact my developer in [support server](${message.client.config.links.support}). Thank your for your patience and understanding.`
-            );
-          }
-    }
-}
+    rsnchat
+      .gpt(message)
+      .then(async (response) => {
+        if (response.success) {
+          const embed = message.client.simpleEmbed({
+            author: true,
+            description: response.message,
+          });
+          message.reply({ embeds: [embed] });
+        } else {
+          await message.reply(
+            `${message.client.emoji.warn} | I am sorry, I could not connect to my chat API. This might be due to a network issue or a temporary outage. Please try again later. You may contact my developer in [support server](${message.client.config.links.support}). Thank your for your patience and understanding.`
+          );
+        }
+      })
+      .catch(async (err) => {
+        console.error(err);
+        await message.reply(
+          `${message.client.emoji.warn} | I am sorry, something went wrong. This might due to errors in my code or an API error. Please try again later or contact my developer in [support server](${message.client.config.links.support}). Thank your for your patience and understanding.`
+        );
+      });
+  },
+};
